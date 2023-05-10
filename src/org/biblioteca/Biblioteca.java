b03d87e684a05f2f028764b9da0871bf90000a6a
Luigi213 <reyesgianluigi88@gmail.com>
2023-05-10 14:17:32 +0200

    completed

--- src/org/biblioteca/Biblioteca.java
+++ src/org/biblioteca/Biblioteca.java
@@ -1,5 +1,100 @@
 package org.biblioteca;
 
-public class Biblioteca {
+import java.io.File;
+import java.io.FileWriter;
+import java.util.Scanner;
 
+import org.book.Libro;
+
+public class Biblioteca {
+	private static final String FILENAME = "C:\\Users\\reyes\\tmp\\text.txt";
+	public static void main(String[] args) {
+		setLibroFile();
+		getFile();
+		
+		System.out.println("The end");
+	}	
+	public static void setLibroFile() {
+		Scanner in = new Scanner(System.in);
+		
+		System.out.println("Inserire il numero di libri");
+		int num = in.nextInt();
+		
+		int count = 0;
+		int choise = -1;
+		
+		Libro[] libArr = new Libro[num];
+		
+		while(true) {
+			
+			System.out.println("1 - inserire libro");
+			System.out.println("2 - uscire");
+			choise = in.nextInt();			
+			
+			if (choise < 1 || choise > 2) {
+				
+				System.out.println("Scelta non compresa, ritentare");
+				continue;
+			}
+			if (choise == 2) break;
+			
+			in.nextLine();
+			
+			System.out.println("Nome del libro?");
+			String nome = in.nextLine();
+			System.out.println("Numero di pagine del libro?");
+			int numPag = in.nextInt();
+			in.nextLine();
+			System.out.println("Nome dell'autore del libro?");
+			String autore = in.nextLine();
+			System.out.println("Editoria del libro?");
+			String editoria = in.nextLine();
+			try {
+				Libro libro = new Libro(nome, numPag, autore, editoria);
+				libArr[count++] = libro;
+			} catch (Exception e) {
+				System.err.println("Errore nella lettura del libro, " + e.getMessage());
+			}
+		}
+		
+		
+		
+		try {
+			FileWriter myWriter = new FileWriter(FILENAME);
+			for(int i=0; i<libArr.length; i++) {
+				
+				Libro l = libArr[i];
+				
+				myWriter.write("[" + i + "]" + l + "\n" + "\n");
+				
+			}
+			
+			myWriter.close();
+			
+			
+		} catch (Exception e ) {
+			System.err.println("Errore nella creazione di Libro\n" + e.getMessage());
+		}
+	}
+	public static void getFile() {
+		try {
+			
+			File mioFile = new File(FILENAME);
+			Scanner reader = new Scanner(mioFile);
+			 
+			while (reader.hasNextLine()) {
+				
+			   String data = reader.nextLine();
+			   
+			   
+			   System.out.println("----------------------");
+			   System.out.println(data);
+			}
+
+			reader.close();
+		} catch (Exception e) {
+			
+			System.err.println("Errore nella lettura di nominativi.txt\n" + e.getMessage());
+		}
+	}
 }
--- src/org/book/Libro.java
+++ src/org/book/Libro.java
@@ -1,7 +1,60 @@
 package org.book;
 
 public class Libro {
-	public static void main(String[] args) {
-		
+	private String libro;
+	private int numPag;
+	private String autore;
+	private String editore;
+	
+	public Libro(String libro, int numPag, String autore, String editore) throws Exception {
+		setLibro(libro);
+		setNumPag(numPag);
+		setAutore(autore);
+		setEditore(editore);
+	}
+
+	public String getLibro() {
+		return libro;
+	}
+	public void setLibro(String libro) throws Exception {
+		if(libro.length() == 0) {
+			throw new Exception("Il libro deve avere un titolo");
+		}
+		this.libro = libro;
+	}
+	public int getNumPag() {
+		return numPag;
+	}
+	public void setNumPag(int numPag) throws Exception {
+		if(numPag <= 0) {
+			throw new Exception("Il numero di pagine non e' definito");
+		}
+		this.numPag = numPag;
+	}
+	public String getAutore() {
+		return autore;
+	}
+	public void setAutore(String autore) throws Exception {
+		if(autore.length() == 0) {
+			throw new Exception("Il libro deve avere un titolo");
+		}
+		this.autore = autore;
+	}
+	public String getEditore() {
+		return editore;
+	}
+	public void setEditore(String editore) {
+		this.editore = editore;
+	}
+	public String getFullName() {
+		return "libro: " + getLibro() + "autore: " + getAutore();
+	}
+	
+	@Override
+	public String toString() {
+		return "libro: " + getLibro()
+		 		+ "\nnumero di pagine: " + getNumPag()
+		 		+ "\nautore: " + getAutore()
+		 		+ "\neditore: " + getEditore();
 	}
 }
